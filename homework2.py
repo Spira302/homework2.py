2b5fd035c090bbad8006d763ce9f34c9edb179c2
Spira302 <egortretyakov2020@gmail.com>
2024-07-02 08:49:47 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+complected_homework =  12
+hour_spend = 1.5
+name_course = 'Курс:Python'
+time_for_one_task = 0.125
+print(name_course,'всего задач:', complected_homework,'затрачено часов:',hour_spend,'среднее время выполнения',time_for_one_task)
